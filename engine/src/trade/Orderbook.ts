fdceb70d2923e46162acdd4b67e2530abba6d7f9
Sunny <swarnendughosh05@gmail.com>
2024-08-20 17:56:36 +0530

    Trade Engin Ordrbook added

--- engine/src/trade/Orderbook.ts
+++ engine/src/trade/Orderbook.ts
@@ -1,3 +1,5 @@
+import { BASE_CURRENCY } from "./Engine";
+
 export interface Order {
     price: number;
     quantity: number;
@@ -110,6 +112,97 @@ export class Orderbook {
             executedQty
         };
     }
- 
-    
+
+    matchAsk(order: Order): {fills: Fill[], executedQty: number} {
+        const fills: Fill[] = [];
+        let executedQty = 0;
+        
+        for (let i = 0; i < this.bids.length; i++) {
+            if (this.bids[i].price >= order.price && executedQty < order.quantity) {
+                const amountRemaining = Math.min(order.quantity - executedQty, this.bids[i].quantity);
+                executedQty += amountRemaining;
+                this.bids[i].filled += amountRemaining;
+                fills.push({
+                    price: this.bids[i].price.toString(),
+                    qty: amountRemaining,
+                    tradeId: this.lastTradeId++,
+                    otherUserId: this.bids[i].userId,
+                    markerOrderId: this.bids[i].orderId
+                });
+            }
+        }
+        for (let i = 0; i < this.bids.length; i++) {
+            if (this.bids[i].filled === this.bids[i].quantity) {
+                this.bids.splice(i, 1);
+                i--;
+            }
+        }
+        return {
+            fills,
+            executedQty
+        };
+    }
+
+    //TODO: Can you make this faster? Can you compute this during order matches?
+    getDepth() {
+        const bids: [string, string][] = [];
+        const asks: [string, string][] = [];
+
+        const bidsObj: {[key: string]: number} = {};
+        const asksObj: {[key: string]: number} = {};
+
+        for (let i = 0; i < this.bids.length; i++) {
+            const order = this.bids[i];
+            if (!bidsObj[order.price]) {
+                bidsObj[order.price] = 0;
+            }
+            bidsObj[order.price] += order.quantity;
+        }
+
+        for (let i = 0; i < this.asks.length; i++) {
+            const order = this.asks[i];
+            if (!asksObj[order.price]) {
+                asksObj[order.price] = 0;
+            }
+            asksObj[order.price] += order.quantity;
+        }
+
+        for (const price in bidsObj) {
+            bids.push([price, bidsObj[price].toString()]);
+        }
+
+        for (const price in asksObj) {
+            asks.push([price, asksObj[price].toString()]);
+        }
+
+        return {
+            bids,
+            asks
+        };
+    }
+
+    getOpenOrders(userId: string): Order[] {
+        const asks = this.asks.filter(x => x.userId === userId);
+        const bids = this.bids.filter(x => x.userId === userId);
+        return [...asks, ...bids];
+    }
+
+    cancelBid(order: Order) {
+        const index = this.bids.findIndex(x => x.orderId === order.orderId);
+        if (index !== -1) {
+            const price = this.bids[index].price;
+            this.bids.splice(index, 1);
+            return price
+        }
+    }
+
+    cancelAsk(order: Order) {
+        const index = this.asks.findIndex(x => x.orderId === order.orderId);
+        if (index !== -1) {
+            const price = this.asks[index].price;
+            this.asks.splice(index, 1);
+            return price
+        }
+    }
+
 }
