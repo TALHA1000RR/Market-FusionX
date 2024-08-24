27eb3a9cd86bda4659b1b948a30df7e3421f0b6e
Sunny <swarnendughosh05@gmail.com>
2024-08-24 11:47:51 +0530

    kline and order routr added

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
 import { orderRouter } from "./routes/order";
+import { klineRouter } from "./routes/kline";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/order", orderRouter);
+app.use("/api/v1/klines", klineRouter)
 // app.use("api/v1/depth"depthRouer);
 
 app.listen(3000, () => {
--- api/src/routes/order.ts
+++ api/src/routes/order.ts
@@ -1,8 +1,46 @@
 import { Router } from "express";
+import { RedisManager } from "../RedisManafger";
+import { CANCEL_ORDER } from "../types";
 
 
 export const orderRouter = Router();
 
- orderRouter.post("/", (req, res) => {
-    
+ orderRouter.post("/",  async (req, res) => {
+      const { market, price, quantity, side, userId } = req.body;
+      console.log({market, price, quantity, side, userId});
+
+
+      const response = await RedisManager.getInstance().sendAndAwait({
+        type: "CREATE_ORDER",
+        data: {
+            market,
+            price,
+            quantity,
+            side,
+            userId
+        }
+      });
+      res.json(response.payload);
+
+      orderRouter.delete("/",  async (req, res) => {
+         const {orderId, market} = req.body;
+         const response = await RedisManager.getInstance().sendAndAwait({
+            type: CANCEL_ORDER,
+            data: {
+                orderId,
+                market,
+            }
+         });
+         res.json(response.payload);
+      })
+      orderRouter.get("/", async (req,res) => {
+          const response = await RedisManager.getInstance().sendAndAwait({
+            type: "GET_OPEN_ORDERS",
+            data:{
+                userId: req.body.userId as string,
+                market: req.body.market as string,
+              }
+          });
+      })
  })
+ 
